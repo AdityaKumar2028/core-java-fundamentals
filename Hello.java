ec1597fb40a9905a108758a0926d8aa8c5a88eb3
Aditya Kumar <way2adityakumar@gmail.com>
2026-03-04 22:29:36 +0530

    static blocks

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
 public class Hello {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    static {
+        System.out.println("Static Block Runs 1");
+    }
+
+    static {
+        System.out.println("Static Block runs 2");
     }
 }

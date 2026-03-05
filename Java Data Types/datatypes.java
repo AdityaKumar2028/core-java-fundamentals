4dd5b751f2fd1c0cd48ebf510bd59a5147bf4e58
Aditya Kumar <way2adityakumar@gmail.com>
2026-03-05 14:00:16 +0530

    datatypes

---
+++ Java Data Types/datatypes.java
@@ -0,0 +1,15 @@
+public class datatypes {
+    public static void main(String[] args) {
+        byte a = 0;
+        short b = 150;
+        int c = 1000;
+        long d = 12302;
+        char e = 'a';
+        float f = 1.23f;
+        double g = 1213;
+
+        byte ao = 10;
+        int result = a + ao;
+        System.out.println(result);
+    }
+}

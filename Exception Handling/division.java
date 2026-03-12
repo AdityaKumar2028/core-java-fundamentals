c7e414a152a46140c09200209783f8bc7706fb54
Aditya Kumar <way2adityakumar@gmail.com>
2026-03-12 18:01:55 +0530

    Division by zero

---
+++ Exception Handling/division.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class division {
+    public static void main(String[] args) {
+        int numerator, denominator, result;
+
+        System.out.print("Enter the numerator: ");
+        Scanner obj = new Scanner(System.in);
+        numerator = obj.nextInt();
+        System.out.print("Enter the denominator: ");
+        denominator = obj.nextInt();
+
+        result = numerator / denominator;
+
+        System.out.println("Result is: " + result);
+
+        System.out.println("More code further");
+    }
+}
